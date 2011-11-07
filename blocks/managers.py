19dfa31cf912c57c315890ecef37d03cb46132b0
Douglas Meehan <dmeehan@gmail.com>
2011-11-07 13:18:35 -0500

    added managers

---
+++ blocks/managers.py
@@ -0,0 +1,24 @@
+# blocks/managers.py
+
+from datetime import datetime
+
+from django.db import models
+from django.db.models.query import QuerySet
+
+class EntryMixin(object):
+    def live(self):
+        return self.get_query_set().filter(status=self.model.STATUS_LIVE)
+
+    def featured(self):
+        return self.get_query_set().filter(status=self.model.STATUS_LIVE)\
+                                   .filter(featured=True)
+    def future(self):
+        return self.get_query_set().filter(status=self.model.STATUS_LIVE)\
+                                   .filter(date_published__lte=datetime.now)
+
+class EntryQuerySet(QuerySet, EntryMixin):
+    pass
+
+class EntryManager(models.Manager, EntryMixin):
+    def get_query_set(self):
+        return EntryQuerySet(self.model, using=self._db)
